e5745d14d4e47d410803374e470cbe06bc27db88
Maria <63619962+MarijaAvramovic@users.noreply.github.com>
2025-07-27 13:46:34 -0700

    Test compuret choice

--- game.js
+++ game.js
@@ -2,12 +2,26 @@
 //console.log(choice1.toLowerCase());
 
 
-let choice2 = parseInt(Math.random() * 100);
+let a = function getComputerChoice(){
+        let x = parseInt(Math.random() * 100);
+
+        if (x <= 33){
+            console.log("rock");
+        }
+          else if (x > 33 && x < 66){
+            console.log("paper");
+        }
+          if (x >= 66){
+            console.log("scissors");
+        }
+
+}
+a();
  
 
 
-for (i=1; i<=5; i++ ) {
+/*for (i=1; i<=5; i++ ) {
    let choice1 = prompt("message");
 console.log(choice1.toLowerCase());
-}
+}*/
 
