d3b558173e039ef4d530c22a811505edd1f28db8
Maria <63619962+MarijaAvramovic@users.noreply.github.com>
2025-07-27 13:40:17 -0700

    Test 5x round

--- game.js
+++ game.js
@@ -1,8 +1,13 @@
-const choice1 = prompt("message");
+//let choice1 = prompt("message");
+//console.log(choice1.toLowerCase());
+
+
+let choice2 = parseInt(Math.random() * 100);
+ 
+
+
+for (i=1; i<=5; i++ ) {
+   let choice1 = prompt("message");
 console.log(choice1.toLowerCase());
-
-
-const choice2 = parseInt(Math.random() * 100);
-console.log(choice2);
-
+}
 
