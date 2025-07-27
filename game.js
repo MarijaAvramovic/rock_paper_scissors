81c4a7f6b4cb449843f4046439b98ee9f5b84bed
Maria <63619962+MarijaAvramovic@users.noreply.github.com>
2025-07-27 13:56:11 -0700

    Create f getHumanChoice

--- game.js
+++ game.js
@@ -2,26 +2,31 @@
 //console.log(choice1.toLowerCase());
 
 
-let a = function getComputerChoice(){
+function getComputerChoice(){
         let x = parseInt(Math.random() * 100);
 
         if (x <= 33){
-            console.log("rock");
+              return "rock";
         }
           else if (x > 33 && x < 66){
-            console.log("paper");
+            return "paper";
         }
           if (x >= 66){
-            console.log("scissors");
+            return "scissors";
         }
 
 }
-a();
+let a = getComputerChoice();
+console.log(a);
  
+//users choice
 
+let b = getHumanChoice();
+function getHumanChoice(){
 
-/*for (i=1; i<=5; i++ ) {
-   let choice1 = prompt("message");
-console.log(choice1.toLowerCase());
-}*/
+   let y = prompt("Rock, Paper, Scissors?");
+    return y.toLowerCase();
+}
+console.log(b);
 
+ 
